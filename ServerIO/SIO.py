edf1a9f4495f969d8889154ddfbfb630ace6348c
Nishit Grover <grovernt@mail.uc.edu>
2023-11-29 23:50:18 -0500

    Added comments to SIO.py and server.py

--- ServerIO/SIO.py
+++ ServerIO/SIO.py
@@ -4,7 +4,9 @@ import json
 import datetime
 import socket
 
+# Function to read data from a socket
 def read_socket(conn: socket.socket, buf=b""):
+    # Read the length of the message
     while len(buf) < 4:
         tbuf = conn.recv(1024)
         if len(tbuf) == 0:
@@ -14,7 +16,8 @@ def read_socket(conn: socket.socket, buf=b""):
     num = int.from_bytes(buf[0:4], byteorder='big')
 
     buf = buf[4:]
-
+    
+    # Read the actual message data
     while len(buf) < num:
         tdata = conn.recv(1024)
         if len(tdata) == 0:
@@ -27,27 +30,36 @@ def read_socket(conn: socket.socket, buf=b""):
 
     return msg, False, buf
 
+
+# Send the length of the message
 def send_socket(sock, json_str: str):
     msg = json_str.encode('utf-8')
     total_bytes_sent = 0
     msglen = len(msg).to_bytes(4, byteorder = 'big')
+    
+    
+    # Send the length of the message
     while total_bytes_sent < 4:
         total_bytes_sent +=\
             sock.send(msglen[total_bytes_sent:])
 
     total_bytes_sent = 0
 
+    # Send the actual message data
     while total_bytes_sent < len(msg):
         total_bytes_sent +=\
             sock.send(msg[total_bytes_sent:])
     
 
+# Function to send data through a socket for a specific user
 def send_socket_user(user, json_str: str):
     with user.sock_lock:
         if not user.connected:
             return
         send_socket(user.sock, json_str)
 
+
+# Function to send a status message to a user
 def send_socket_msg(user, msg):
     json_str = {
         "type": "status",
@@ -55,9 +67,11 @@ def send_socket_msg(user, msg):
     }
     send_socket_user(user, json.dumps(json_str))
 
+# Function to send different types of data to a user
 def send_socket_data(type: str, user, data):
     json_msg: str
     match type:
+        # Message or Group Message data
         case "users" | "groupusers":
             data: List[str]
             json_str = {
@@ -70,6 +84,7 @@ def send_socket_data(type: str, user, data):
             data: List
             sanitized_data = []
             try:
+                # format each message in the list
                 for msg in data:
                     sanitized_data.append({
                         "id": msg.id,
@@ -86,14 +101,19 @@ def send_socket_data(type: str, user, data):
             }
             json_msg = json.dumps(json_str)
 
+
+        # Group data
         case "groups":
             data: List
+            # Format group data
             data = [f"Group {i+1} id - {v.id}" for i, v in enumerate(data)]
             json_str = {
                 "type": "groups",
                 "data": data
             }
             json_msg = json.dumps(json_str)
+    
+    # Send the formatted data to the user
     send_socket_user(user, json_msg)
 
 """
--- server/server.py
+++ server/server.py
@@ -2,6 +2,16 @@ import socket, threading, select
 from typing import List
 
 class Server:
+    """
+        Initialize the Server object.
+
+        Parameters:
+        - HOST: The host address to bind the server to.
+        - PORT: The port number to bind the server to.
+        - HANDLER: The handler function to be executed for each incoming connection.
+        - MAX_BACKLOG: Maximum number of queued connections (default is 5).
+        """
+        
     def __init__(self, HOST, PORT, HANDLER, MAX_BACKLOG = 5):
         self.HOST = HOST
         self.PORT = PORT
@@ -13,6 +23,9 @@ class Server:
         self.__running__: bool = False
 
     def start(self):
+        """
+        Start the server by creating a socket, binding to the specified address and port,and listening for incoming connections. Also, start a separate thread to handle incoming connections concurrently.
+        """
         if self.__running__:
             raise RuntimeError("Server already started")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,6 +38,9 @@ class Server:
         self.__listener__.start()
 
     def __listen_and_disbatch(self):
+        """
+        Listen for incoming connections and dispatch them to handler threads.
+        """
         while self.__running__:
             readable, writable, errored = select.select([self.socket], [], [], 1)
             if self.socket not in readable:
@@ -37,6 +53,9 @@ class Server:
                 self.running_handlers.append(ct)
 
     def stop(self):
+        """
+        Stop the server by setting the running flag to False, joining the listener thread, and joining all the running handler threads.
+        """
         self.__running__ = False
         if self.__listener__.is_alive():
             self.__listener__.join()
@@ -44,8 +63,14 @@ class Server:
             i.join()
 
     def __enter__(self):
+        """
+        Enter method for context management. Start the server when entering a block.
+        """
         self.start()
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Exit method for context management. Stop the server when exiting a block.
+        """
         self.stop()
 
